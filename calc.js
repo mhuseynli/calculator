d961f8a7b728c734d24eeb0d5dc1296cfa6af52e
Murad Huseynli <muradhuseynli570@gmail.com>
2020-08-03 12:31:39 +0400

    someedits

--- calc.js
+++ calc.js
@@ -1,7 +1,12 @@
-function remove() {
-  //   const strng = document.calc.display.value;
-  //   document.calc.display.value = strng.substring(0, strng.length - 1);
-  const strng = document.calc.display.value;
-  document.calc.display.value = strng.slice(0, -1);
-}
+// function remove() {
+//   //   const strng = document.calc.display.value;
+//   //   document.calc.display.value = strng.substring(0, strng.length - 1);
+//   const strng = document.calc.display.value;
+//   document.calc.display.value = strng.slice(0, -1);
+// }
 
+$(document).ready(function () {
+  $(".backspace").click(function () {
+    $(".display").val($(".display").val().slice(0, -1));
+  });
+});
